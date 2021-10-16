3f320abc60f65638a2d4a16cb17e8ad4d39e9d91
Diego Beutler <diegobeutler2411@gmail.com>
2021-10-16 19:50:49 -0300

    Initial commit

---
+++ desktop/src/br/edu/utfpr/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package br.edu.utfpr.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import br.edu.utfpr.showDoMilhao;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new showDoMilhao(), config);
+	}
+}
