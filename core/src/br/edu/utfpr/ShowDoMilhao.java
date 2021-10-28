1d37d71aac48429bd2f87f8e1c205e6bb96833d2
ryancamaargo <ryancamaargo@hotmail.com>
2021-10-27 22:25:04 -0300

    bullet e pooling, button aparecendo mas nao pega a ação

--- core/src/br/edu/utfpr/ShowDoMilhao.java
+++ core/src/br/edu/utfpr/ShowDoMilhao.java
@@ -5,24 +5,11 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.assets.AssetManager;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.ImageTextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
-import com.badlogic.gdx.utils.Align;
-import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
 
 public class ShowDoMilhao extends Game {
     public static ShowDoMilhao game;
-    public  AssetManager assetManager;
+    public AssetManager assetManager;
 
     public static InputMultiplexer multiplexer;
 
@@ -38,11 +25,6 @@ public class ShowDoMilhao extends Game {
 
     public void create() {
         assetManager = new AssetManager();
-//		batch = new SpriteBatch();
-//		Sound sound = Gdx.audio.newSound(Gdx.files.internal("sons\\vaiComecarOShowDoMilhao.mp3"));
-//		sound.play(1f);
-//		img = new Texture("imagens\\show-do-milhao.jpg");
-//		MainScreen mainScreen = new MainScreen();
         this.setScreen(new LoadingScreen(this));
         game = this;
     }
