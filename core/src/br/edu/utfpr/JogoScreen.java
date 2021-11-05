721ed3d316d7118c3c46b22b252cc3fc936b2a3d
Diego Beutler <diegobeutler2411@gmail.com>
2021-11-05 19:45:32 -0300

    leitura do json e sorteio das questoes

--- core/src/br/edu/utfpr/JogoScreen.java
+++ core/src/br/edu/utfpr/JogoScreen.java
@@ -1,39 +1,29 @@
 package br.edu.utfpr;
 
 import br.edu.utfpr.jogo.Jogo;
-import br.edu.utfpr.jogo.Rodada;
 import br.edu.utfpr.json.Dados;
 import br.edu.utfpr.json.Questao;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.assets.AssetManager;
-import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.google.gson.Gson;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import static br.edu.utfpr.jogo.Jogo.getJogo;
-import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
-
-import java.awt.*;
-
-import com.badlogic.gdx.graphics.Color;
 public class JogoScreen implements Screen {
     private ShowDoMilhao showDoMilhao;
 
@@ -45,6 +35,16 @@ public class JogoScreen implements Screen {
     public Moeda moeda;
     private OrthographicCamera camera;
     private Viewport viewport;
+    private boolean emPergunta;
+    Questao  questao;
+    Dados dados;
+    Jogo jogo;
+    private BitmapFont font1 = new BitmapFont();
+    private BitmapFont font2 = new BitmapFont();
+    private BitmapFont font3 = new BitmapFont();
+    private BitmapFont font4 = new BitmapFont();
+    private BitmapFont font5 = new BitmapFont();
+
 
     public static JogoScreen ref;
     public JogoScreen() {
@@ -65,9 +65,9 @@ public class JogoScreen implements Screen {
                             Paths.get(url),
                             StandardCharsets.UTF_8)
             );
-            Dados dados = new Gson().fromJson(json, Dados.class);
-            Jogo jogo = getJogo();
-            Questao questao = dados.getQuestao(jogo.getDificuldade());
+            dados = new Gson().fromJson(json, Dados.class);
+            jogo = getJogo();
+            questao = dados.getQuestao(jogo.getDificuldade());
             System.out.println(questao.getKey());
         } catch (Exception e) {
             e.printStackTrace();
@@ -81,6 +81,7 @@ public class JogoScreen implements Screen {
 
         camera.position.set(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2, 0);
         viewport = new FitViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), camera);
+        questao = dados.getQuestao(jogo.getDificuldade());
 
     }
 
@@ -99,17 +100,40 @@ public class JogoScreen implements Screen {
 
         stage.draw();
 
+
+
         //forma
 
+//        ShapeRenderer shape = new ShapeRenderer();
+//        shape.setProjectionMatrix(camera.combined);
+//        shape.begin(ShapeRenderer.ShapeType.Filled);
+//        shape.setColor(Color.WHITE);
+//        shape.rect(15, (Gdx.graphics.getHeight()/2)-15, Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
+//        shape.end();
 
+        font1.draw(batch, questao.getPergunta(), 50, 520);
+        font1.getData().setScale((float) 2, (float) 2);
+        font1.setColor(Color.BLACK);
+
+        font2.draw(batch, questao.getRespostas().get(0).getResposta(), 50, 450);
+        font2.getData().setScale((float) 2, (float) 2);
+        font2.setColor(Color.GRAY);
+
+        font3.draw(batch, questao.getRespostas().get(1).getResposta(), 50, 400);
+        font3.getData().setScale((float) 2, (float) 2);
+        font3.setColor(Color.GRAY);
+
+        font4.draw(batch, questao.getRespostas().get(2).getResposta(), 50, 350);
+        font4.getData().setScale((float) 2, (float) 2);
+        font4.setColor(Color.GRAY);
+
+        font5.draw(batch, questao.getRespostas().get(3).getResposta(), 50, 300);
+        font5.getData().setScale((float) 2, (float) 2);
+        font5.setColor(Color.GRAY);
         batch.end();
 
-        ShapeRenderer shape = new ShapeRenderer();
-        shape.setProjectionMatrix(camera.combined);
-        shape.begin(ShapeRenderer.ShapeType.Filled);
-        shape.setColor(Color.WHITE);
-        shape.rect(15, (Gdx.graphics.getHeight()/2)-15, Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
-        shape.end();
+
+
 
 //        ShapeRenderer shape2 = new ShapeRenderer();
 //        shape2.setProjectionMatrix(camera.combined);
